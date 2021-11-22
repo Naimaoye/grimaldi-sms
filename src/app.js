ec8e00d1a6c4c3e9c273800996b049d7e8f15d78
user <naimat.oyewale@digitalpulse.ng>
2021-11-22 10:58:26 +0000

    implement two-way sms grimaldi

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 import express from 'express'
 require('./config/redis-config');
-import ussdRoute from './route';
+import smsRoute from './route';
 import { logger } from './config/loggerConfig';
 
 const app = express();
 
 const PORT = 9840;
 
-app.use(ussdRoute);
+app.use(smsRoute);
 
 app.listen(PORT, () => {
     logger.info(`App running on port ${PORT}`);
